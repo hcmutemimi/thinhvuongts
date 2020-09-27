463ded4986d2d1502233ad8b2fef57802a3d0b34
hcmutemimi <hoaimiqng@gmail.com>
2020-09-27 13:46:25 +0700

    complete

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
 document.addEventListener("DOMContentLoaded", function(){
     $(window).scroll(function () { 
-        //Position:
-        var position = $('.partners__item').position();
-        console.log(position);
-        var position = $('.counselors__left-image--item').position();
-        console.log(position);
         
-        //đếm class Name partners__item
-        var nodelist = document.getElementsByClassName('partners__item').length;
-        console.log(nodelist);
-
-        window.addEventListener('scroll',function(){
-
+        var position = $('.partners__item').position(); // lấy vị trí
+        // console.log(position);
+       
+        var position = $('.organizational-chart--image').position();
+        var counselorsleftimage = document.querySelector('.organizational-chart--image');
+      
         var partnerimage = document.querySelectorAll('.partners__item');
-            if(window.pageXOffset = 3614)
-                for (let i = 0; i < 3; i++) 
-                    partnerimage[i].classList.add('on-show')
-            else if(window.pageXOffset = 3934)
-                    for (let i = 3; i < 6; i++) 
-                        partnerimage[i].classList.add('on-show')
-            else if(window.pageXOffset = 4254)
-                    for (let i = 6; i < 9; i++) 
-                        partnerimage[i].classList.add('on-show')          
-          })
-          window.addEventListener('scroll',function(){
-          var counselorsleftimage =  document.querySelector('.counselors__left-image--item');
-          if(window.pageXOffset = 4704) {
-                counselorsleftimage.classList.add('on-show')
-          }
-          });
+       
+        // window.addEventListener('scroll',function(){
+        //     if(window.scrollX = 2781) {
+        //         counselorsleftimage.classList.add('on-show');
+        //     }
+        //     if(window.scrollX = 3614)
+        //     {
+        //         for (var i = 0; i < 3; i++){
+        //             partnerimage[i].classList.add('on-show')
+        //         }
+        //         setTimeout(function () {
+        //             for (var i = 3; i < 6; i++){
+        //                 partnerimage[i].classList.add('on-show')
+        //             }
+        //           }, 800);
+                
+        //           setTimeout(function () {
+        //             for (var i = 6; i < 9; i++){
+        //                 partnerimage[i].classList.add('on-show')
+        //             }
+        //           }, 1600); 
+           
+        //         }
+
+               
+      
+        //   })
     });
 })
+// Wow JS
 new WOW().init();
+AOS.init();
